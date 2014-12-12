afd4bdd7bb3e253418db0b463baa50ebc532191a
tanxunrong <tanxrong@gmail.com>
2014-12-12 23:40:50 +0800

    fix warning

--- cfuncs.go
+++ cfuncs.go
@@ -5,6 +5,8 @@ package duktape
 #cgo CFLAGS: -std=c99 -I./
 #include "duktape.h"
 
+extern void go_duktape_fatal(duk_context *ctx, duk_errcode_t code,const char* msg);
+
 void go_duktape_fatal_cgo(duk_context *ctx, duk_errcode_t code,const char* msg) {
 	go_duktape_fatal(ctx,code,msg);
 }
